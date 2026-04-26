8fcbb612db8e10b0e56eb94d490c047a9b82a71d
Francesco Beretta <francesco.beretta@cnrs.fr>
2026-04-26 10:19:29 +0200

    acm clusters

--- documentation/wikidata/data-analysis/da5-explore-clusters.sql
+++ documentation/wikidata/data-analysis/da5-explore-clusters.sql
@@ -1,10 +1,22 @@
 
+select *
+from ACM_kmeans_c21
+where cluster= 17
+order by gender, periodsActivity;
+
+
 SELECT *
 FROM clusters_kmodes ck 
-WHERE ck.cluster = 4 ; -- 7;
+WHERE ck.cluster = 2 ; -- 7;
 
 
 SELECT gender, count(*) as num
 FROM clusters_kmodes ck 
-WHERE ck.cluster = 5 --0  --7
+WHERE ck.cluster = 2 --0  --7
 group by gender;
+
+SELECT ck.coded_country , count(*) as num
+FROM clusters_kmodes ck 
+WHERE ck.cluster = 2 --0  --7
+group by coded_country 
+order by num desc;
